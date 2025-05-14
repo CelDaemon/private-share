d1bf1e2aee24601767d8419c2e74e51bcf6dd094
CelDaemon <celdaemon@voidgroup.net>
2025-05-14 21:08:05 +0200

    Bump to 0.3 and make listed

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ echo
 read -sp "Enter secret:" secret
 echo
 
-WEB_EXT_API_KEY="$key" WEB_EXT_API_SECRET="$secret" npx web-ext sign -s src/ -a out --channel unlisted
+WEB_EXT_API_KEY="$key" WEB_EXT_API_SECRET="$secret" npx web-ext sign -s src/ -a out --channel listed
